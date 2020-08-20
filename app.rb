50ccede79a5b94035b03289009d4bc58e4e64153
Adam Palmer <theadampalmer@gmail.com>
2020-08-20 18:18:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		"<h1>Hello World</h1>"
+		erb :index
 	end
+
+	get '/info' do
+		erb :info
+	end 
 	
-	get '/' do
-      erb :index
-  end
- 
-  get "/info" do
-      erb :info
-  end
-  
 end
